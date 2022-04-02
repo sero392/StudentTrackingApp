2f50cf5e5fa153d1d3457b2d46e9e6fedc448ddd
Serhan <serhann798@gmail.com>
2022-04-02 12:23:55 +0300

    proje oluşturma

---
+++ student-tracking-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-main>
+      <MessageBox ref="msgBox"></MessageBox>
+      <helloWorld :run="openMenu"></helloWorld>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld  from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  methods: {
+    openMenu: function () {
+      var th = this;
+      var dialog = th.$refs.msgBox;
+      console.log(dialog);
+      dialog.isOpen = true;
+    },
+  },
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ student-tracking-app/src/components/MessageBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-dialog persistent v-model="isOpen" width="500">
+    <v-card>
+      <v-card-title>Remove</v-card-title>
+      <v-card-text>Are you sure to delete?</v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" text @click="$emit('closedialog')">Close</v-btn>
+        <!-- <v-btn color="primary" text @click="deleteItem">Delete</v-btn>  -->
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+export default {
+  name: 'MessageBox',
+  data: () => ({
+    isOpen: false,
+  }),
+};
+</script>
+<style scoped>
+</style>
